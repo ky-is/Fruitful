ffc0af85c2daff3d32a2dab80ee1c29232af5dcf
Kyle <kylecoburn@gmail.com>
2024-01-15 10:21:28 -0800

    ♻️ Habit model extensions

--- Extensions/Foundation+Extensions.swift
+++ Extensions/Foundation+Extensions.swift
@@ -1,4 +1,17 @@
+import CoreGraphics
 import Foundation
+
+extension CGColor {
+	var hexColor: UInt {
+		let r = components?[0] ?? 0
+		let g = components?[1] ?? 0
+		let b = components?[2] ?? 0
+		let red = UInt(r * 255) << 16
+		let green = UInt(g * 255) << 08
+		let blue = UInt(b * 255)
+		return red | green | blue
+	}
+}
 
 extension TimeInterval {
 	static let second: Self = 1
--- Models/Habit.swift
+++ Models/Habit.swift
@@ -1,65 +1,8 @@
 import SwiftData
 import SwiftUI
 
-enum HabitInterval: Codable, CaseIterable, CustomStringConvertible {
-	case day, week, month, year
-
-	var description: String {
-		switch self {
-		case .day: "daily"
-		case .week: "weekly"
-		case .month: "monthly"
-		case .year: "yearly"
-		}
-	}
-
-	var duration: TimeInterval {
-		switch self {
-		case .day: .day
-		case .week: .week
-		case .month: .day * 28
-		case .year: .day * 365
-		}
-	}
-
-	var startDate: Date {
-		let calendar = Calendar.current
-		let now = Date()
-		return switch self {
-//		case .hour: calendar.date(bySettingHour: calendar.component(.hour, from: startDate), minute: 0, second: 0, of: startDate)!
-		case .day: calendar.startOfDay(for: now)
-		case .week: calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: now))!
-		case .month: calendar.date(from: calendar.dateComponents([.year, .month], from: now))!
-		case .year: calendar.date(from: calendar.dateComponents([.year], from: now))!
-		}
-	}
-
-	func getEndDate(from startDate: Date) -> Date {
-		let addComponent: Calendar.Component = switch self {
-//		case .hour: .hour
-		case .day: .day
-		case .week: .weekOfMonth
-		case .month: .month
-		case .year: .year
-		}
-		return Calendar.current.date(byAdding: addComponent, value: 1, to: startDate)!
-	}
-}
-
-extension CGColor {
-	var hexColor: UInt {
-		let r = components?[0] ?? 0
-		let g = components?[1] ?? 0
-		let b = components?[2] ?? 0
-		let red = UInt(r * 255) << 16
-		let green = UInt(g * 255) << 08
-		let blue = UInt(b * 255)
-		return red | green | blue
-	}
-}
-
 @Model
-final class Habit: Comparable {
+final class Habit {
 	var title = ""
 	var createdAt = Date.now
 	var icon = ""
@@ -72,18 +15,6 @@ final class Habit: Comparable {
 	var goalCount: Int = 1
 
 	var hexColor: UInt = 0
-	var cgColor: CGColor {
-		get {
-			CGColor(srgbRed: Double((hexColor >> 16) & 0xff) / 255, green: Double((hexColor >> 08) & 0xff) / 255, blue: Double((hexColor >> 00) & 0xff) / 255, alpha: 1)
-		}
-		set {
-			hexColor = newValue.hexColor
-		}
-	}
-	var color: Color {
-		Color(cgColor: cgColor)
-	}
-
 
 	var completedUntil = Date.now
 	var completedAt: Date?
@@ -108,11 +39,22 @@ final class Habit: Comparable {
 		self.goalLabel = goalLabel
 		self.goalCount = goalCount
 		self.hexColor = hexColor ?? .random(in: 999999...999999999)
+
 		self.completedAt = completedAt
 	}
+}
 
-	static func < (lhs: Habit, rhs: Habit) -> Bool {
-		lhs.title < rhs.title
+extension Habit {
+	var cgColor: CGColor {
+		get {
+			CGColor(srgbRed: Double((hexColor >> 16) & 0xff) / 255, green: Double((hexColor >> 08) & 0xff) / 255, blue: Double((hexColor >> 00) & 0xff) / 255, alpha: 1)
+		}
+		set {
+			hexColor = newValue.hexColor
+		}
+	}
+	var color: Color {
+		Color(cgColor: cgColor)
 	}
 
 	func updateCompleted(newCount: Int) {
@@ -129,5 +71,78 @@ final class Habit: Comparable {
 			completedCount -= 1
 			completedStreak -= 1 //TODO
 		}
+	}
+}
+
+extension Habit: Comparable {
+	static func < (lhs: Habit, rhs: Habit) -> Bool {
+		lhs.title < rhs.title
+	}
+}
+
+enum HabitPriority: Codable, CaseIterable, CustomStringConvertible {
+	case low, normal, high
+
+	var description: String {
+		switch self {
+		case .low: "low"
+		case .normal: "normal"
+		case .high: "important"
+		}
+	}
+
+	var icon: String {
+		switch self {
+		case .low: "sleep" // "arrow.down.circle"
+		case .normal: "wake" // "circle.and.line.horizontal" // "circle.bottomhalf.filled"
+		case .high: "exclamationmark.circle"
+		}
+	}
+}
+
+enum HabitInterval: Codable, CaseIterable, CustomStringConvertible {
+	case hour, day, week, month, year
+
+	var description: String {
+		switch self {
+		case .hour: "daily"
+		case .day: "daily"
+		case .week: "weekly"
+		case .month: "monthly"
+		case .year: "yearly"
+		}
+	}
+
+	var duration: TimeInterval {
+		switch self {
+		case .hour: .hour
+		case .day: .day
+		case .week: .week
+		case .month: .day * 28
+		case .year: .day * 365
+		}
+	}
+
+	var startDate: Date {
+		let calendar = Calendar.current
+		let now = Date()
+		return switch self {
+		case .hour: calendar.date(bySettingHour: calendar.component(.hour, from: now), minute: 0, second: 0, of: now)!
+		case .day: calendar.startOfDay(for: now)
+		case .week: calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: now))!
+		case .month: calendar.date(from: calendar.dateComponents([.year, .month], from: now))!
+		case .year: calendar.date(from: calendar.dateComponents([.year], from: now))!
+		}
+	}
+
+	func getEndDate(from startDate: Date) -> Date {
+		let addComponent: Calendar.Component = switch self {
+		case .hour: .hour
+		case .day: .day
+		case .week: .weekOfMonth
+		case .month: .month
+		case .year: .year
+		}
+		return Calendar.current.date(byAdding: addComponent, value: 1, to: startDate)!
 	}
 }
